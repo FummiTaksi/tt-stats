c62d4d2c2e16f15d0361df3f2f2efe3f006b3cae
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2022-10-09 13:16:47 +0300

    Add Player Model (#4)
    * WIP create class for player
    
    * remove player folder
    
    * Implement players into src folder
    
    * Add database seeds
    
    * add documentation about migrations and seeding players
    
    * add newline at end of file

---
+++ database-seeds/insert-players.sql
@@ -0,0 +1,2 @@
+insert into player (name) values ('John Doe');
+insert into player (name) values ('Richard Doe');
--- tt-stats-backend/backend/backend/schema.py
+++ tt-stats-backend/backend/backend/schema.py
@@ -1,10 +1,20 @@
 import graphene
+from graphene_django import DjangoObjectType
+from .src.player import Player
+
+class PlayerType(DjangoObjectType):
+    class Meta:
+        model = Player
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
+    players = graphene.List(PlayerType)
 
     def resolve_hello(self, resolve, **kwargs):
         return 'Hello World!'
+
+    def resolve_players(self, resolve, **kwargs):
+        return Player.objects.all()
 
 
 schema = graphene.Schema(query=Query)
