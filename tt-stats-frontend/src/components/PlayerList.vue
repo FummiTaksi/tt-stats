48352185ee3a27922812580600065ed35a686d26
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2022-12-11 11:54:26 +0200

    implement PlayerList component

---
+++ tt-stats-frontend/src/components/PlayerList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  props: {
+    ['players']: { type: Array, required: true },
+  },
+}
+</script>
+
+<template>
+  <li v-for="{ name, id } in players" v-bind:key="id">
+    {{ name }}
+  </li>
+</template>
--- tt-stats-frontend/src/views/PlayersView.vue
+++ tt-stats-frontend/src/views/PlayersView.vue
@@ -5,20 +5,20 @@
     <p v-if="loading">Loading...</p>
     <div v-else>
       <h1>The database contains {{ result.players.length }} players</h1>
-      <li v-for="{ name, id } in result.players" v-bind:key="id">
-        {{ name }}
-      </li>
+      <PlayerList :players="result.players" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { usePlayersQuery } from '../generated/graphql'
+import PlayerList from '../components/PlayerList.vue'
 
 export default {
   setup() {
     const { result, loading, error } = usePlayersQuery()
     return { result, loading, error }
   },
+  components: { PlayerList },
 }
 </script>
