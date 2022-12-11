df07bc84a60daab8f84929ae5e7c94d2a5e60872
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2022-12-11 11:02:46 +0200

    Add view which lists players

--- tt-stats-frontend/src/router/index.ts
+++ tt-stats-frontend/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PlayersView from '../views/PlayersView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/players',
+    name: 'players',
+    component: PlayersView,
   },
   {
     path: '/about',
---
+++ tt-stats-frontend/src/views/PlayersView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="apollo">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <p v-if="error">Something went wrong...</p>
+    <p v-if="loading">Loading...</p>
+    <div v-else>
+      <h1>The database contains {{ result.players.length }} players</h1>
+      <li v-for="{ name, id } in result.players" v-bind:key="id">
+        {{ name }}
+      </li>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { usePlayersQuery } from '../generated/graphql'
+
+export default {
+  setup() {
+    const { result, loading, error } = usePlayersQuery()
+    return { result, loading, error }
+  },
+}
+</script>
