990f7dc4eabca570af21036aa756a21e313d4bde
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2023-01-15 13:27:00 +0200

    add view for creating a match

--- tt-stats-frontend/src/App.vue
+++ tt-stats-frontend/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/players">Players</router-link>
+    <router-link to="/players">Players</router-link> |
+    <router-link to="/matches">Matches</router-link>
   </nav>
   <router-view />
 </template>
---
+++ tt-stats-frontend/src/views/CreateMatchView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="apollo">
+    <p v-if="error">Something went wrong...</p>
+    <p v-if="loading">Loading...</p>
+    <p v-if="creatingMatch">Creating match...</p>
+    <div v-else>
+      <div>
+        Select winner
+        <select v-model="winningPlayer">
+          <option disabled value="">Please select one</option>
+          <option
+            v-for="player in result.players"
+            v-bind:key="player.id"
+            v-bind:value="{ id: player.id, name: player.name }"
+          >
+            {{ player.name }}
+          </option>
+        </select>
+      </div>
+      <div>
+        Select loser
+        <select v-model="losingPlayer">
+          <option disabled value="">Please select one</option>
+          <option
+            v-for="player in result.players"
+            v-bind:key="player.id"
+            v-bind:value="{ id: player.id, name: player.name }"
+          >
+            {{ player.name }}
+          </option>
+        </select>
+      </div>
+      <button
+        @click="
+          createMatch({
+            winnerId: winningPlayer.id,
+            loserId: losingPlayer.id,
+          })
+        "
+        v-if="losingPlayer !== null && winningPlayer !== null"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { usePlayersQuery, useCreateMatchMutation } from '../generated/graphql'
+
+export default {
+  setup() {
+    const { result, loading, error } = usePlayersQuery()
+    const {
+      mutate: createMatch,
+      loading: creatingMatch,
+      error: createMatchError,
+    } = useCreateMatchMutation()
+    return {
+      result,
+      loading,
+      error,
+      createMatch,
+      creatingMatch,
+      createMatchError,
+    }
+  },
+  data() {
+    return {
+      winningPlayer: null,
+      losingPlayer: null,
+    }
+  },
+}
+</script>
