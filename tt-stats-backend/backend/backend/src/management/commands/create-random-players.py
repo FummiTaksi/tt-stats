cd723f161d236a8421b842337cc15602ed1a9dbc
Aleksi Mustonen <aleksi.mustonen@wunderdog.fi>
2023-03-26 20:52:50 +0300

    remove unused variable

--- tt-stats-backend/backend/backend/src/management/commands/create-random-players.py
+++ tt-stats-backend/backend/backend/src/management/commands/create-random-players.py
@@ -5,17 +5,10 @@ from django.core.management.base import BaseCommand, CommandError
 def create(amount):
 
     for x in range(amount):
-        name_legth = 7
-
         random_name = names.get_full_name()
-
         print('Creating player with name', random_name)
-
         Player.objects.create(name=random_name)
-
         print(str(x + 1) + ' players created')
-
-
 
 class Command(BaseCommand):
     help = 'Creates player with random name to the database'
